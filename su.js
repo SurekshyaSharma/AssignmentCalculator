e491015b5fab984fad6c9ea31f3fc291cd7ca019
[Surekshya Sharma] <[xst-shar3415@stthomas.edu]>
2019-08-06 15:46:24 -0500

    fixed the bug for the date calculation

--- su.js
+++ su.js
@@ -4,75 +4,7 @@ $(document).ready( function () {
     dateDiffInDays()
    
 });
-
-function AssignCalculator(){
-
-      //   startMonth = document.getElementById("userStartMonth").value;
-      //   startDay = document.getElementById("userStartDay").value;
-      //   startYear = document.getElementById("userStartYear").value;
-      //   console.log( startMonth, startYear);
-
-      //   submitMonth = document.getElementById("userCloseMonth").value;
-      //   submitDay = document.getElementById("userCloseDay").value;
-      //   submitYear = document.getElementById("userCloseYear").value;
-      //   console.log('Day:',submitDay,'Month:', submitMonth,'Year:',submitYear);
-
-        /*************INput type**************************************************************** */
-      
-        assignmentType = document.getElementById("assignType").value;
-    
-        if (  assignmentType == "Annotated Bibliography")
-        {
-          $("#submitResult").empty();
-            
-                    $("#submitResult").append(
-                         '<div>'+
-                         '<p> Note: This is how you start</p>'+  '<br>'+
-                        ' `<a href="https://writingcenter.unc.edu/tips-and-tools/understanding-assignments/" target="_blank">Read through the assignment</a>`'+
-                     '</div>'
-                        )
-         
-        }
-        if (  assignmentType == "Business Report")
-        {
-          $("#submitResult").empty();
-
-             $("#submitResult").append(
-               '<div>'+
-               '<p>'+ 'Note: A business report is an evaluation or assessment of a particular issue or set of circumstances'+
-               'or financial operations that relate to the performance of a business'+'</p>'+  '<br>'+
-           '`<a href="https://writingcenter.unc.edu/tips-and-tools/understanding-assignments/" target="_blank">How to write a Business Paper</a>`'+
-           '</div>'
-              )
-         
-        }
-
-        if (  assignmentType == "Research Paper")
-        {
-          $("#submitResult").empty();
-
-               $("#submitResult").append(
-               '<div>'+
-               '<p>'+' Note: The research and writing process is not always linear. Keep in mind you may need to go forward or backwards.'+'</p>'+  '<br>'+
-               '`<a href="https://writingcenter.unc.edu/tips-and-tools/understanding-assignments/" target="_blank">Documents on how to start Research Paper</a>`'+
-           '</div>'
-              )
-
-        }
-       
-        if (  assignmentType == "Reflection Paper")
-        {
-          $("#submitResult").empty();
-            
-             $("#submitResult").append(
-               '<div>'+
-               '<p>'+ 'Note: A reflection paper is your chance to add your thoughts and analysis to what you have read and experienced. A reflection paper is meant to illustrate your understanding of the material and how it affects your ideas and possible practice in future.</p>'+  '<br>'+
-               '`<a href="https://www.wikihow.com/Write-a-Reflection-Paper" target="_blank">Take some google help</a>`'+
-           '</div>'
-              )
-
-        }
-    }         
+  
    
     function dateDiffInDays()
     {
@@ -93,12 +25,6 @@ function AssignCalculator(){
          }
          console.log(date_diff_indays(start, submit));
       /**********************************************************Checking if o difference dont show any dates***************************************************************************************************************** */
-        
-   
-            // ($("#daysLeft").empty());
-      
-            // $("#daysLeft").append(
-            //    '<p>'+'Days Left for submission:'+ '</p>')
       
                if (date_diff_indays(start, submit)!= 0){
       
@@ -111,22 +37,83 @@ function AssignCalculator(){
             if(date_diff_indays(start, submit)<0) {
 	
                $("#daysLeft").append(
-                  "<b>Negative Number of Days:</b> Probably you entered the dates wrong...\n")
+                  "<b>Negative Number of Days:</b> Probably you entered the dates wrong...")
             
             }else if(date_diff_indays(start, submit)<0)  {
             
-               $("#daysLeft").append("Hurry up !!!<br You have less than a week. />\n")
+               $("#daysLeft").append("Hurry up !!! You have less than a week.")
             
             
             }else if(date_diff_indays(start, submit)>7) {
             
-            	$("#daysLeft").append("Looks like you have enough time.<br>Let's plan accordingly.\n")
+            	$("#daysLeft").append("Looks like you have enough time.Let's plan accordingly.")
             
             }
             
             if(date_diff_indays(start, submit)>99) {
-               $("#daysLeft").append( "It's never too early to start!<br />\n")
+               $("#daysLeft").append( "It's never too early to start!")
             }
-            
-           AssignCalculator();
+
+            assignmentType = document.getElementById("assignType").value;
+    
+            if (  assignmentType == "Annotated Bibliography")
+            {
+               $("#submitResult").empty();
+               $("#submitResult").show();
+                
+               $("#submitResult").append(
+                     ' <br></br>'+
+                     '<div>'+
+                     '<p> Note: This is how you start</p>'+  '<br>'+
+                     ' `<a href="https://writingcenter.unc.edu/tips-and-tools/understanding-assignments/" target="_blank">Read through the assignment</a>`'+
+                  '</div>'
+                     )
+      
+            }
+            if (  assignmentType == "Business Report")
+            {
+              $("#submitResult").empty();
+              $("#submitResult").show();
+    
+              $("#submitResult").append(
+                   ' <br></br>'+
+                   '<div>'+
+                   '<p>'+ 'Note: A business report is an evaluation or assessment of a particular issue or set of circumstances'+
+                   'or financial operations that relate to the performance of a business'+'</p>'+  '<br>'+
+               '`<a href="https://writingcenter.unc.edu/tips-and-tools/understanding-assignments/" target="_blank">How to write a Business Paper</a>`'+
+               '</div>'
+                  )
+             
+            }
+    
+            if (  assignmentType == "Research Paper")
+            {
+              $("#submitResult").empty();
+              $("#submitResult").show();
+    
+              $("#submitResult").append(
+             ' <br></br>'+
+             '<div>'+
+             '<p>'+' Note: The research and writing process is not always linear. Keep in mind you may need to go forward or backwards.'+'</p>'+  '<br>'+
+             '`<a href="https://writingcenter.unc.edu/tips-and-tools/understanding-assignments/" target="_blank">Documents on how to start Research Paper</a>`'+
+             '</div>'
+         )
+    
+            }
+           
+            if (  assignmentType == "Reflection Paper")
+            {
+              $("#submitResult").empty();
+              $("#submitResult").show();
+                
+              $("#submitResult").append(
+               ' <br></br>'+
+               '<div>'+
+               '<p>'+ 'Note: A reflection paper is your chance to add your thoughts and analysis to what you have read and experienced. A reflection paper is meant to illustrate your understanding of the material and how it affects your ideas and possible practice in future.</p>'+  '<br>'+
+               '`<a href="https://www.wikihow.com/Write-a-Reflection-Paper" target="_blank">Take some google help</a>`'+
+               '</div>'
+               )
+    
+            }
+
     }
